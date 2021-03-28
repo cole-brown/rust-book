7429aa0926a6308a647932362d570021dfeb26d7
Cole Brown <code@brown.dev>
2021-03-28 16:51:16 -0700

    Finished Ch. 03-01 to 03-04. Started 03-05.

--- projects/03-01_variables/variables/src/main.rs
+++ projects/03-01_variables/variables/src/main.rs
@@ -1,6 +1,15 @@
+const MAX_POINTS: u32 = 100_000;
+
 fn main() {
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    let y = 55;
+    println!("The value of y is: {}", y);
+    let y = y + 1;
+    println!("The value of y is: {}", y);
+    let y = y * 2;
+    println!("The value of y is: {}", y);
 }
